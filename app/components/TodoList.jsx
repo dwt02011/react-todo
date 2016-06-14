3224c7360c26a4fc63d1331cc045049814b7f84d
Daniel Thompson <daniel.thompson@prepfactory.com>
2016-06-14 15:43:51 -0400

    Wire up Todo toggle

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -2,12 +2,15 @@ var React = require('react');
 var Todo = require('Todo');
 
 var TodoList = React.createClass({
+	propTypes: {
+		onToggle: React.PropTypes.func.isRequired
+	},
 	render: function () {
 		var {todos} = this.props;
 		var renderTodos = () => {
 			return todos.map((todo) => {
 				return (
-					<Todo key={todo.id} {...todo}/>
+					<Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/>
 				);
 			});
 		};
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -21,4 +21,18 @@ describe('TodoApp', () => {
 		expect(todoApp.state.todos.length).toBe(1);
 		expect(todoApp.state.todos[0].text).toBe(todoText);
 	});
+
+	it('should toggle completed value when handleToggle called', () => {
+		var id = 11;
+		var todoData = {
+			id: id, text: 'test', completed: false
+		};
+
+		var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+		todoApp.setState({todos: [todoData]});
+
+		expect(todoApp.state.todos[0].completed).toBe(false);
+		todoApp.handleToggle(id);
+		expect(todoApp.state.todos[0].completed).toBe(true);	
+	});
 });
