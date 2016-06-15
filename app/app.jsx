cdedd99175fbbb9d7452dd24ee9c750221e26028
Daniel Thompson <daniel.thompson@prepfactory.com>
2016-06-15 17:26:01 -0400

    Update todos location to account for uid

--- app/app.jsx
+++ app/app.jsx
@@ -13,14 +13,13 @@ store.subscribe(() => console.log('State changed:', store.getState()));
 firebase.auth().onAuthStateChanged((user) => {
 	if (user) {
 		store.dispatch(actions.login(user.uid));
+		store.dispatch(actions.startAddTodos());
 		hashHistory.push('/todos');
 	} else {
 		store.dispatch(actions.logout());
 		hashHistory.push('/');
 	}
 });
-
-store.dispatch(actions.startAddTodos());
 
 // Load foundation
 $(document).foundation();
