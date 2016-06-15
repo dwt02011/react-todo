159821cfccefd8805b506a08669cbac90bf85841
Daniel Thompson <daniel.thompson@prepfactory.com>
2016-06-15 10:48:24 -0400

    Add todos reducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
 export var searchTextReducer = (state = '', action) => {
 	switch (action.type) {
 	case 'SET_SEARCH_TEXT':
@@ -15,3 +18,36 @@ export var showCompletedReducer = (state = false, action) => {
 		return state;
 	}
 };
+
+export var todosReducer = (state = [], action) => {
+	switch (action.type) {
+	case 'ADD_TODO':
+		return [
+			...state,
+			{
+				id: uuid(),
+				text: action.text,
+				completed: false,
+				createdAt: moment().unix(),
+				completedAt: undefined
+			}
+		];
+	case 'TOGGLE_TODO':
+		return state.map(todo => {
+			var nTodo = todo;
+			if(nTodo.id === action.id) {
+				var newCompleted = !todo.completed;
+
+				return {
+					...todo,
+					completed: newCompleted,
+					completedAt: newCompleted ? moment().unix() : undefined
+				};
+			} else {
+				return nTodo;
+			}
+		});
+	default:
+		return state;
+	}
+}
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,4 +1,5 @@
 var expect = require('expect');
+// verify that reducer methods don't modify inputs
 var df = require('deep-freeze-strict');
 var reducers = require('reducers');
 
@@ -23,6 +24,61 @@ describe('Reducers', () => {
 			var res = reducers.showCompletedReducer(df(false), df(action));
 
 			expect(res).toEqual(true);
+		});
+	});
+
+	describe('todosReducer', () => {
+		it('should add new todo', () => {
+			var action = {
+				type: 'ADD_TODO',
+				text: 'Walk the dog'
+			};
+
+			var res = reducers.todosReducer(df([]), df(action));
+
+			expect(res.length).toEqual(1);
+			expect(res[0].id).toBeA('string');
+			expect(res[0].text).toEqual(action.text);
+		});
+
+		var todos = [{
+			id: '1',
+			text: 'Incomplete todo',
+			completed: false,
+			createdAt: 12345,
+			completedAt: undefined
+		}, {
+			id: '2',
+			text: 'Completed todo',
+			completed: true,
+			createdAt: 12345,
+			completedAt: 23456
+		}];
+		
+		it('should toggle appropriate todo from incomplete to complete', () => {
+			var action = {
+				type: 'TOGGLE_TODO',
+				id: '1'
+			}
+
+			expect(todos[0].completed).toBe(false);
+			expect(todos[0].completedAt).toBe(undefined);
+			var res = reducers.todosReducer(df(todos), df(action));
+			expect(res[0].completed).toBe(true);
+			expect(res[0].completedAt).toBeA('number');			
+		});
+
+		it('should toggle appropriate todo from incomplete to complete', () => {
+			var action = {
+				type: 'TOGGLE_TODO',
+				id: '2'
+			}
+
+			expect(todos[1].completed).toBe(true);
+			expect(todos[1].completedAt).toBeA('number');
+			var res = reducers.todosReducer(df(todos), df(action));
+			expect(res[1].completed).toBe(false);
+			expect(res[1].completedAt).toBe(undefined);			
 		});
 	});
 });
